cf02eefb1950f36e5a2dabe2de2f65975080c99f
droid-janderson <jandersongit@gmail.com>
2025-09-01 06:43:42 -0300

    feat(header): implement header component with navigation

---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar :color="theme.current.value.colors.background" :elevation="2">
+    <v-app-bar-title
+      class="font-weight-bold"
+      :style="{
+        color: theme.current.value.colors.primary
+      }"
+    >CoinVertire <v-icon color="secondary">mdi-bitcoin</v-icon></v-app-bar-title>
+    <v-btn
+      :color="theme.current.value.dark ? 'secondary' : 'primary'"
+      :icon="theme.current.value.dark ? 'mdi-white-balance-sunny' : 'mdi-weather-night'"
+      @click="theme.toggle()"
+    />
+  </v-app-bar>
+</template>
+
+<script setup>
+  import { useTheme } from 'vuetify'
+
+  const theme = useTheme()
+</script>
