9535bc17b92e21e6b6e664976ac79b8983628ef3
droid-janderson <jandersongit@gmail.com>
2025-09-01 06:52:18 -0300

    feat(home): implements main page

---
+++ src/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <container
+    class="d-flex align-center justify-space-around mt-4"
+    style="min-height: 90vh"
+  >
+    <div
+      class="d-flex flex-column align-center justify-center rounded-xl"
+      :style="{
+        width: '500px',
+        height: '460px',
+        backgroundColor: theme.current.value.colors.backgroundSec,
+        'box-shadow': theme.current.value.colors.boxShadow,
+      }"
+    >
+      <form-converter />
+    </div>
+  </container>
+</template>
+
+<script lang="ts" setup>
+  import { useTheme } from 'vuetify'
+
+  const theme = useTheme()
+</script>
